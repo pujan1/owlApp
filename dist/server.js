b0feafb2ea93db91d5f801dcd3be4368b660c9b7
Pujan <pujan3892@gmail.com>
2018-06-14 22:58:04 -0500

    Fix: Cleanup

--- dist/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// lib/server.ts
-const app_1 = require("./app");
-const PORT = 3000;
-app_1.default.listen(PORT, () => {
-    console.log('Express server listening on port ' + PORT);
-});
-//# sourceMappingURL=server.js.map
