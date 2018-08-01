b84f25cdd88d1559aa05dfe9cb67516bc089e757
Pujan <pujan3892@gmail.com>
2018-07-31 23:50:21 -0500

    Fix: Fetch match data from server rather then form the decprcartng API

--- lib/controllers/matchStats/owl-match-stats.controller.ts
+++ lib/controllers/matchStats/owl-match-stats.controller.ts
@@ -1,14 +1,24 @@
 import { Request, Response } from 'express';
 import * as ExtReq from 'request'
+import * as mongoose from "mongoose";
+
 
 export class OwlMatchStatsController {
   public getMatchStats(req: Request, res: Response) {
 
-    ExtReq.get(' https://api.overwatchleague.com/stats/matches/'+ req.params.id +'/maps/' + req.params.number, (error, response, body) => {
+    //ExtReq.get(' https://api.overwatchleague.com/stats/matches/'+ req.params.id +'/maps/' + req.params.number, (error, response, body) => {
 
-      const data = JSON.parse(body)
-      res.json(data);
-    }) 
+      //const data = JSON.parse(body)
+     // res.json(data);
+
+      mongoose.connect("mongodb://pujan:pujan123@ds249311.mlab.com:49311/owlapp", function(err, db) {
+        const collection = db.collection('matches');
+
+          collection.find().toArray(function(err, kittens) {
+              console.log('kittens', kittens[0].game_number);
+          });    
+      });
+    //}) 
    }
 }
 
