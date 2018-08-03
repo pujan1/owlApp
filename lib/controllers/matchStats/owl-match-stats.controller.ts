7bc56c329469385880de69986a1941dd1d314747
Pujan <pujan3892@gmail.com>
2018-08-02 23:24:23 -0500

    Fix: APi and mongoose fix for match individual response

--- lib/controllers/matchStats/owl-match-stats.controller.ts
+++ lib/controllers/matchStats/owl-match-stats.controller.ts
@@ -3,22 +3,32 @@ import * as ExtReq from 'request'
 import * as mongoose from "mongoose";
 
 
+
 export class OwlMatchStatsController {
+
+  constructor() {
+    const Schema = mongoose.Schema;
+    mongoose.connect("mongodb://pujan:pujan123@ds249311.mlab.com:49311/owlapp", function(err, db) {
+       const collection = db.collection('matches');
+    });
+  }
+
+
+  
   public getMatchStats(req: Request, res: Response) {
 
-    //ExtReq.get(' https://api.overwatchleague.com/stats/matches/'+ req.params.id +'/maps/' + req.params.number, (error, response, body) => {
+    this.collection.find({game_number: 4},(kittens) =>  {
+      res.send(kittens);
+    }); 
+    
+      // mongoose.connect("mongodb://pujan:pujan123@ds249311.mlab.com:49311/owlapp", function(err, db) {
+      //   console.log(db);
+      //   const collection = db.collection('matches');
+      //     collection.find({game_number: 4},(kittens) =>  {
+      //         res.send(kittens);
+      //     }); 
+      // })
 
-      //const data = JSON.parse(body)
-     // res.json(data);
-
-      mongoose.connect("mongodb://pujan:pujan123@ds249311.mlab.com:49311/owlapp", function(err, db) {
-        const collection = db.collection('matches');
-
-          collection.find().toArray(function(err, kittens) {
-              console.log('kittens', kittens[0].game_number);
-          });    
-      });
-    //}) 
    }
 }
 
