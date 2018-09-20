7b7bc3945e16a89ac4d85abf83eae34b6178e1cb
Pujan <pujan3892@gmail.com>
2018-09-19 22:09:59 -0500

    I AM BACK: Feat: Let the node serve the front end build

--- lib/app.ts
+++ lib/app.ts
@@ -1,7 +1,7 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import { Routes } from "./routes/owl.routing";
-import * as mysql from 'mysql';
+//import * as mysql from 'mysql';
 
 
 class App {
@@ -19,21 +19,22 @@ class App {
   private config(): void{
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: false }));
+    this.app.use(express.static(__dirname+'/dist'));
   }
 
-  private configureSQL(): void{
-    let connection = mysql.createConnection({
-      host: '35.226.98.99',
-      user: 'pujan',
-      database: 'owlapp',
-      password: 'pujan123'
-    });
+  // private configureSQL(): void{
+  //   let connection = mysql.createConnection({
+  //     host: '35.226.98.99',
+  //     user: 'pujan',
+  //     database: 'owlapp',
+  //     password: 'pujan123'
+  //   });
 
-    connection.connect( (err) => {
-      if (err) console.error('Error connecting: ' + err.stack);
-      console.log('Connected as thread id: ' + connection.threadId);
-    });
-  }
+  //   connection.connect( (err) => {
+  //     if (err) console.error('Error connecting: ' + err.stack);
+  //     console.log('Connected as thread id: ' + connection.threadId);
+  //   });
+  // }
 }
 
 export default new App().app;
