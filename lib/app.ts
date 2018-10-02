13fbddada5543568903a23b5e6cc1f666468659a
Pujan <pujan3892@gmail.com>
2018-10-02 00:37:34 -0500

    error handling and fix progress for routes.

--- lib/app.ts
+++ lib/app.ts
@@ -1,6 +1,7 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as path from "path"
+import * as morgan from "morgan"
 import { Routes } from "./routes/owl.routing";
 //import * as mysql from 'mysql';
 
@@ -13,19 +14,14 @@ class App {
   constructor() {
     this.app = express();
     this.config();  
-    this.app.use('/api', this.route);
-    //this.route.routes(this.app);    
+    this.route.routes(this.app);    
   }
 
   private config(): void{
     this.app.use(bodyParser.json());
+    this.app.use(morgan('dev'));
     this.app.use(bodyParser.urlencoded({ extended: false }));
     this.app.use(express.static(__dirname+'/dist'));
-    // this.app.all('/*',  (req, res, next) => {
-    //   // Just send the index.html for other files to support HTML5Mode
-    //   res.sendFile('index.html', { root: __dirname+'/dist' });
-    // });
-
     this.app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, 'dist/index.html'));
     });
--- lib/routes/owl.routing.ts
+++ lib/routes/owl.routing.ts
@@ -1,4 +1,4 @@
-
+import * as express from 'express';
 import { OwlStandingsController } from "../controllers/standings/owl-standings.controller";
 import { OwlScheduleController } from "../controllers/schedule/owl-schedule.controller";
 import { OwlMatchStatsController } from '../controllers/matchStats/owl-match-stats.controller'
@@ -7,29 +7,33 @@ import { OwlStatsController } from "../controllers/stats/owl-stats.controller";
 
 export class Routes {   
     
-    public standingsController: OwlStandingsController = new OwlStandingsController();
-    public scheduleController: OwlScheduleController = new OwlScheduleController();
-    public matchStatsController: OwlMatchStatsController = new OwlMatchStatsController();
-    public playerStatsController: OwlPlayerStatsController = new OwlPlayerStatsController();
-    public statsController: OwlStatsController = new OwlStatsController();
+	public standingsController: OwlStandingsController = new OwlStandingsController();
+	public scheduleController: OwlScheduleController = new OwlScheduleController();
+	public matchStatsController: OwlMatchStatsController = new OwlMatchStatsController();
+	public playerStatsController: OwlPlayerStatsController = new OwlPlayerStatsController();
+	public statsController: OwlStatsController = new OwlStatsController();
 
 
-    public routes(app): void {   
+	public routes(app) {   
+		const router = express.Router();
 
-    app.route('/api/standings')
-        .get(this.standingsController.getStandings);
 
-    app.route('/api/schedule')
-        .get(this.scheduleController.getSchedule);
+		router.route('/standings')
+				.get(this.standingsController.getStandings);
 
-    app.route('/api/match/:id/:number')
-        .get(this.matchStatsController.getMatchStats)
-    
-    app.route('/api/player/:id')
-        .get(this.playerStatsController.getMatchStats)
-    
-    app.route('/api/stats/:type')
-        .get(this.statsController.getStats)
-        
-    }
+		router.route('/schedule')
+				.get(this.scheduleController.getSchedule);
+
+		router.route('/match/:id/:number')
+				.get(this.matchStatsController.getMatchStats)
+		
+		router.route('/player/:id')
+				.get(this.playerStatsController.getMatchStats)
+		
+		router.route('/stats/:type')
+				.get(this.statsController.getStats)
+
+		app.use('/api', router);
+			
+	}
 }
